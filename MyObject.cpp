871c94f82286af8c40f17a813597f8a121c22ff6
Russell Richardson <Russell@KronosAD.com>
2014-04-03 02:00:21 -0500

    Create Unstable branch containing MyObject

---
+++ MyObject.cpp
@@ -0,0 +1,15 @@
+#import "MyObject.h"
+using namespace std;
+
+	string testString;
+	MyObject::MyObject(std::string test){
+
+		this->testString = test;
+	}
+
+	string MyObject::getTestString(){
+
+		return this->testString;
+
+	}
+
---
+++ MyObject.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string>
+using namespace std;
+class MyObject{
+public:
+	string testString;
+	string getTestString();
+	MyObject(string);
+
+private:
+
+};
