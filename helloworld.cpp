41bc817d5739d8bb4a116660e85e41566505c595
Russell Richardson <Russell@KronosAD.com>
2014-04-03 01:23:29 -0500

    Initial Commit Kappa

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,17 +7,24 @@ using namespace std;
 // returns status
 int main(int argc, char const *argv[]) {
 	
+	cout << "attempting to turn it off on and on again" << endl;
+
 	cout << "primary expression Kappa" << endl;
 	for(int i = 0; i < argc; i++) {
 		cout << argv[i] << endl; //DONE
  	}
 	cout << endl;
 	//what is the meaning of life?
-	cout << "The meaning of life is obviously " << 42 << endl;
-	for(int x = 0; x < 10; x++){
-		for(int y = 0; y < 10; y++){
-			cout << x + y << endl;
-		}
+	cout << "The meaning of life is obviously " << 47 << endl;
+	cout << endl << "Let's do some weird stuff" << endl;
+	int startX = 0;
+	int startY = 0;
+	int limitX = 500;
+	int limitY = 500;
+	int thingX = 0;
+	int thingY = 0;
+	for(int i = 0; i < 500; i++) {
+		
 	}
 	cout << "HEHEHEHEHEH" << endl;
 	return 0;
