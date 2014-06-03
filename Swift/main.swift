4a29438eef7d31670e4bbbdadc370608fb7fbf3f
Russell Richardson <Russell@KronosAD.com>
2014-06-02 23:06:33 -0500

    Because I can.

---
+++ Swift/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  SwiftTesting
+//
+//  Created by Russell Richardson on 6/2/14.
+//  Copyright (c) 2014 Russell Richardson. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, Swift")
+
+var myStr = "Roosall Rikurdsun"
+
+println(myStr)
+
+var myDict = Dictionary<String, String>()
+
+myDict["WTF"] = "Swift"
+
+println("WTF " + myDict["WTF"]!)
+
+let individualScores = [75, 43, 103, 87, 12]
+
+var teamScore = 0
+
+
